2de517b61f71385a8621c4c9cd3d2724209660ef
Виктория Бабынцева <deathrabbitzzz@gmail.com>
2018-08-03 13:14:40 +0000

    Парсинг с помощью двух библиотек-nokogiri+CURL

--- Parsing.php
+++ Parsing.php
@@ -1,22 +1,23 @@
 <?php
 require '../vendor/autoload.php';
+require 'QueryingDatabase.php';
+//numberPages-колличество страниц
+$numberPages=5;
 
-$parsingName='.post__title_link';
-$parsingData='.post__time';
-$parsingNumberOfComments='.post-stats__comments-count';
-
-parsingFunction($parsingData,5);
-parsingFunction($parsingName,5);
-parsingFunction($parsingNumberOfComments,5);
-
-///
-function parsingFunction ($url,$numberPages)
+//ПРИМЕЧАНИЕ!
+//Цикл используется для прохода по страницам хабра
+ for ($number=1; $number<$numberPages+1; $number++)
+ {
+	 //hostname-адрес удаленного сервера
+	 $hostname ='https://habr.com/all/page'.$number."/";
+	 get_content($hostname);	
+ }
+ 
+// Скрипт парсинга определенных классов (ищет + выводит на экран) с помощью библиотеки nokogiri
+ function parsingFunction ($url,$content)
 {
-	for ($number=1; $number<$numberPages+1; $number++)
-	{
-		$file='https://habr.com/all/page'.$number."/";
-		$html=file_get_contents($file);
-		$doc=new nokogiri($html);
+		
+		$doc=new nokogiri($content);
 		echo '<pre>';
 			foreach($doc->get($url)->toArray() as $item)
 			{ 
@@ -24,7 +25,39 @@ function parsingFunction ($url,$numberPages)
 				echo '<br>';
 			}	
 		echo '</pre>';
-	}
+
 }
 
+//Скрипт загрузки удаленной страницы средствами
+//библиотеки CURL + функция parsingFunction
+ function get_content($hostname)
+ {
+	 //Ключевые адреса поиска классов
+	 //parsingName- классов "Заголовки статей"
+	 //parsingData- классов "Даты публикаци статей"
+	 //parsingNumberOfComments- классов "Колличества комментариев статей"
+			$parsingName='.post__title_link';
+			$parsingData='.post__time';
+			$parsingNumberOfComments='.post-stats__comments-count';
+			
+	//задание адреса серва
+			$curl = curl_init($hostname);
+			
+ 
+			curl_setopt($curl, CURLOPT_URL,$hostname);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
+			curl_setopt($curl, CURLOPT_HEADER, 0);
+
+			$content = curl_exec($curl);
+			
+	// Закрытие CURL-соединение
+			curl_close($curl);
+			
+	//парсинг заголовков,даты,комментариев
+			parsingFunction($parsingName,$content);
+			parsingFunction($parsingData,$content);
+			parsingFunction($parsingNumberOfComments,$content);
+			return $content;
+ }
+ 
 ?>
